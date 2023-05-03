e42f08140668a50ef0953d79efc631a9312dc946
Alex Wellnitz <moin@wellnitz-alex.de>
2023-05-03 14:11:16 +0200

    Add plugins and color schema

---
+++ plugins/todo-comments.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {}
+    end,
+  },
+}
---
+++ plugins/tokyonight.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    as = "tokyonight",
+    config = function()
+      require("tokyonight").setup {}
+    end,
+  },
+}
